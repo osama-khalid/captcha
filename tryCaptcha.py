35b89bc0213dc4ac0e29e96088f73e8716b8cb00
osama-khalid <khalyd.osama@gmail.com>
2022-06-17 06:34:05 -0700

    Add files via upload

---
+++ tryCaptcha.py
@@ -0,0 +1,30 @@
+import requests
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import requests
+import time
+
+pageurl = 'https://www.google.com/recaptcha/api2/demo'
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
+
+d = DesiredCapabilities.CHROME
+d['loggingPrefs'] = { 'performance':'ALL' }
+
+
+driver = webdriver.Chrome(desired_capabilities=d, executable_path='./chromedriver')
+
+
+driver.header_overrides = {
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+}
+driver.get(pageurl)
+
+WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe[name^='a-'][src^='https://www.google.com/recaptcha/api2/anchor?']")))
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@id='recaptcha-anchor']"))).click()
+time.sleep(10)
